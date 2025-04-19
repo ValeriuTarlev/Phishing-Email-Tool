8494cd61acaa77994f5de384fc23f701c219866c
Valeriu <valeriu/tarlev17@gmail.com>
2025-04-19 02:42:11 -0500

    Add domain check

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from phishing_rules import check_keywords
+from phishing_rules import domain_sender_check
 
 sample_email = 'Urgent! A new document has to be signed. It need immediate action invoice'
 
 result = check_keywords(sample_email)
 print('suspicious words found:', result)
 
+suspiscious_domain = "vale@google.com"
 
-
-
+results = domain_sender_check(suspiscious_domain)
+print(results)
