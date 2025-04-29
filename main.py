16f8517e4628cd4cbb9665a488818038c08f5bc3
Valeriu <valeriu/tarlev17@gmail.com>
2025-04-29 02:06:29 -0500

    Reorganize main script to process multiple sender emails and links

--- main.py
+++ main.py
@@ -1,12 +1,30 @@
-from phishing_rules import check_keywords
-from phishing_rules import domain_sender_check
+from phishing_rules import check_keywords, domain_sender_check, check_suspicious_link
 
+# Sample email text
 sample_email = 'Urgent! A new document has to be signed. It need immediate action invoice'
 
-result = check_keywords(sample_email)
-print('suspicious words found:', result)
+# Check for phishing words
+keywords_found = check_keywords(sample_email)
+print('suspicious words found:', keywords_found)
 
-suspiscious_domain = "vale@google.com"
 
-results = domain_sender_check(suspiscious_domain)
-print(results)
+suspicious_senders = [
+    "vale@google.com",
+    "admin@secure-amazon.tk",
+    "support@gma1l.com",
+]
+
+for sender in suspicious_senders: 
+    result = domain_sender_check(sender)
+    print(f"Domain check for {sender}: {result}")
+
+
+suspicious_link = [
+    "http://192.168.0.1/login",
+    "https://www.google.com",
+    "http://bit.ly/phishing-link",
+]
+
+for link in suspicious_link:
+    link_result = check_suspicious_link(link) 
+    print(f"Link check for {link}: {link_result}")
